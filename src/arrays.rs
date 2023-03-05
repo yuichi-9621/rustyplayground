864058706a7803c3cdc442e9fde562f2dca4b71f
yuichiokuhama <yuokuhama@gmail.com>
2023-03-05 00:08:52 -0800

    Array

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,10 +1,12 @@
 // Arrays - Fixed list where elements are the same data types
+use std::mem;
 
 pub fn run() {
     let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
 
     //use debug traits to show array
     println!("{:?}", numbers);
+    println!("Array length: {}", numbers.len());
 
     //get the first value
     println!("First num is: {}", numbers[0]);
@@ -13,4 +15,13 @@ pub fn run() {
     numbers[0] = 25;
     println!("First num is: {} now", numbers[0]);
     println!{"{:?}", numbers};
+
+    //Arrays are stack allocated
+    println!("Arrays occupies {} bytes", mem::size_of_val(&numbers));
+
+    //Get Slice
+    let slice: &[i32] = &numbers[0..2];
+    println!("Slice: {:?}", slice); //printing out array so :?
+
+    //println!("Slice the first 3: {}" )
 }
